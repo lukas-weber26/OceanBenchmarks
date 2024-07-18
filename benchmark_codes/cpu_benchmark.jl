edc484b4e1eb4ffb853209783b4b05ddd322731b
Lukas <lukas.weber26@gmail.com>
2024-07-18 02:48:19 +0000

    Better benchmarks, benchmark source code, submission script, three dimensional codes.

---
+++ benchmark_codes/cpu_benchmark.jl
@@ -0,0 +1,66 @@
+push!(LOAD_PATH, joinpath(@__DIR__, ".."))
+
+using BenchmarkTools
+using CUDA
+using Oceananigans
+using Oceananigans.Utils: oceananigans_versioninfo, versioninfo_with_gpu
+using CairoMakie
+using Dates
+
+#anything that you may wish to modify goes here
+gridSizes = [(32, 32), (64, 64), (128, 128), (256, 256), (512, 512), (1024, 1024), (2048, 2048), (4096, 4096), (8192,8192), (16384,16384)]
+
+#helper functions 
+function writeTrial(B)
+
+        date = string(Dates.now())
+        callName = "Oceananigans_Benchmark:" * date * ".csv"
+
+        open(callName, "w") do io
+                write(io, "Model,GridType,GridSize,MinTime,MedianTime,MeanTime,MaxTime,Memory,Allocs,NumSamples")
+                for trialDict in B
+                        t = trialDict["trial"]
+			trialInfo = trialDict["model"] * "," * trialDict["gridType"] * "," * trialDict["gridSize"][1] * "," * string(minimum(t.times)) * "," * string(median(t.times)) * "," * string(mean(t.times)) * "," * string(maximum(t.times)) * "," * string(t.memory) * "," * string(t.allocs) * "," * string(t.params.samples) * "\n"
+
+                        write(io, trialInfo)
+                end
+        end
+end
+
+#main code 
+completedBenchmarks = []
+for m in ["Hydrostatic", "Nonhydrostatic", "ShallowWater"]
+        for g in gridSizes
+                println("Starting Case ", string(g))
+
+                if (m == "Nonhydrostatic")
+                        grid = RectilinearGrid(size=g, x=(0, 2 * 3.14), y=(0, 2 * 3.14), topology=(Periodic, Periodic, Flat))
+                        model = NonhydrostaticModel(; grid, advection=WENO())
+
+                        e(e, y) = 2rand() - 1
+                        set!(model, u=e, v=e)
+                elseif (m == "Hydrostatic")
+                        grid = RectilinearGrid(size=(g[1], g[2], 1), extent=(1, 1, 1))
+                        model = HydrostaticFreeSurfaceModel(; grid)
+
+                elseif (m == "ShallowWater")
+                        grid = RectilinearGrid(size=g, x=(0, 2 * 3.14), y=(0, 2 * 3.14), topology=(Periodic, Periodic, Flat), halo=(3, 3))
+                        model = ShallowWaterModel(grid=grid, gravitational_acceleration=1.0)
+                        set!(model, h=1)
+                else
+                        println("Invalid choice of model")
+                        break
+                end
+
+                trial = @benchmark begin
+                        time_step!($model, 1)
+                end samples = 10
+
+                push!(completedBenchmarks, Dict("trial" => trial, "model" => m, "gridType" => "RectilinearGrid", "gridSize" => string(g))
+                )
+        end
+end
+
+writeTrial(completedBenchmarks)
+
+
---
+++ benchmark_codes/sample_job.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --mail-user=YOUR_EMAIL
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --job-name="Oceananigans_benchmark"
+#SBATCH --partition=gpu_a100 
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=12
+#SBATCH --time=1:00:00
+#SBATCH --mem-per-cpu=2000
+#SBATCH --gres=gpu:a100:1
+#SBATCH --output=%x_%j.out
+#SBATCH --error=%x_%j.err
+
+echo "Cuda device: $CUDA_VISIBLE_DEVICES"
+echo "======= Start memory test ======="
+julia /work/PATH_TO_BENCHMARK
+echo "done"
