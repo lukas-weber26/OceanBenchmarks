4586b5e5c7c01bde8685ec63af1ebd6a417d0049
Lukas <lukas.weber26@gmail.com>
2024-07-23 17:18:53 +0000

    Added a previously hidden detail about gpu memory to the submission script. Turns out this resolves all kinds of issues...

--- benchmark_codes/sample_job.sh
+++ benchmark_codes/sample_job.sh
@@ -9,6 +9,7 @@
 #SBATCH --time=1:00:00
 #SBATCH --mem-per-cpu=2000
 #SBATCH --gres=gpu:a100:1
+#SBATCH --mem-per-gpu=80G  <MAKE SURE TO ADJUST THIS NUMBER>
 #SBATCH --output=%x_%j.out
 #SBATCH --error=%x_%j.err
 
