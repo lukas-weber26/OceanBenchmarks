ab3d56c6ac96f3bf47f40e40bc7160143f8ea9c5
Lukas <lukas.weber26@gmail.com>
2024-08-08 18:26:56 +0000

    one less point

--- new_benchmarks/3d_plotter.py
+++ new_benchmarks/3d_plotter.py
@@ -8,11 +8,11 @@ hydrostatic_a100 = data_a100[(data_a100["Model"] == "Hydrostatic")]
 data_h100 = pd.read_csv("./H100_3d.csv", delimiter=",",index_col=None)
 hydrostatic_h100 = data_h100[(data_h100["Model"] == "Hydrostatic")]
 
-grid_sizes = np.array([64,128,256,512])
+grid_sizes = np.array([128,256,512])
 
 #comparison of the gpus
-plt.loglog(grid_sizes, hydrostatic_a100["MeanTime"][1:])
-plt.loglog(grid_sizes, hydrostatic_h100["MeanTime"][1:])
+plt.loglog(grid_sizes, hydrostatic_a100["MeanTime"][2:])
+plt.loglog(grid_sizes, hydrostatic_h100["MeanTime"][2:])
 plt.title("Hydrostatic Model A100 vs H100 3D")
 plt.xlabel("Grid Size")
 plt.ylabel("Mean Time")
