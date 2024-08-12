923897b9889e3e961f9e5fd5c9ee1a5a771326c5
Lukas <lukas.weber26@gmail.com>
2024-08-12 21:56:49 +0000

    Some potential fixes for three dimensional benchmarks

--- benchmark_codes/3dcodes/cpu_benchmark_3d.jl
+++ benchmark_codes/3dcodes/cpu_benchmark_3d.jl
@@ -7,8 +7,7 @@ using Oceananigans.Utils: oceananigans_versioninfo, versioninfo_with_gpu
 using CairoMakie
 using Dates
 
-#anything that you may wish to modify goes here
-gridSizes = [(32, 32, 32), (64, 64, 64), (128, 128, 128), (256, 256, 256), (512, 512, 512)]
+gridSizes = [32, 64, 128, 256, 512]
 
 #helper functions 
 function writeTrial(B)
@@ -20,7 +19,7 @@ function writeTrial(B)
                 write(io, "Model,GridType,GridSize,MinTime,MedianTime,MeanTime,MaxTime,Memory,Allocs,NumSamples\n")
                 for trialDict in B
                         t = trialDict["trial"]
-                        trialInfo = trialDict["model"] * "," * trialDict["gridType"] * "," * trialDict["gridSize"][1] * "," * string(minimum(t.times)) * "," * string(median(t.times)) * "," * string(mean(t.times)) * "," * string(maximum(t.times)) * "," * string(t.memory) * "," * string(t.allocs) * "," * string(t.params.samples) * "\n"
+                        trialInfo = trialDict["model"] * "," * trialDict["gridType"] * "," * trialDict["gridSize"] * "," * string(minimum(t.times)) * "," * string(median(t.times)) * "," * string(mean(t.times)) * "," * string(maximum(t.times)) * "," * string(t.memory) * "," * string(t.allocs) * "," * string(t.params.samples) * "\n"
 
                         write(io, trialInfo)
                 end
@@ -34,21 +33,19 @@ for m in ["Hydrostatic", "Nonhydrostatic", "ShallowWater", "ShallowWaterConserva
                 println("Starting Case ", string(g))
 
                 if (m == "Nonhydrostatic")
-                        grid = RectilinearGrid(size=g, x=(0, 2 * 3.14), y=(0, 2 * 3.14), topology=(Periodic, Periodic, Flat))
+                        grid = RectilinearGrid(size=(g, g, g), x=(0, 2 * 3.14), y=(0, 2 * 3.14), z=(0, 2 * 3.14), topology=(Periodic, Periodic, Bounded))
                         model = NonhydrostaticModel(; grid, advection=WENO())
-
                         e(e, y) = 2rand() - 1
                         set!(model, u=e, v=e)
                 elseif (m == "Hydrostatic")
-                        grid = RectilinearGrid(size=(g[1], g[2], 1), extent=(1, 1, 1))
+                        grid = RectilinearGrid(size=(g, g, g), x=(0, 2 * 3.14), y=(0, 2 * 3.14), z=(0, 2 * 3.14), topology=(Periodic, Periodic, Bounded))
                         model = HydrostaticFreeSurfaceModel(; grid)
-
                 elseif (m == "ShallowWater")
-                        grid = RectilinearGrid(size=g, x=(0, 2 * 3.14), y=(0, 2 * 3.14), topology=(Periodic, Periodic, Flat), halo=(3, 3))
+                        grid = RectilinearGrid(size=(g, g, g), x=(0, 2 * 3.14), y=(0, 2 * 3.14), topology=(Periodic, Periodic, Bounded), halo=(3, 3))
                         model = ShallowWaterModel(grid=grid, gravitational_acceleration=1.0, formulation=VectorInvariantFormulation())
                         set!(model, h=1)
                 elseif (m == "ShallowWaterConservative")
-                        grid = RectilinearGrid(size=g, x=(0, 2 * 3.14), y=(0, 2 * 3.14), topology=(Periodic, Periodic, Flat), halo=(3, 3))
+                        grid = RectilinearGrid(size=(g, g, g), x=(0, 2 * 3.14), y=(0, 2 * 3.14), topology=(Periodic, Periodic, Bounded), halo=(3, 3))
                         model = ShallowWaterModel(grid=grid, gravitational_acceleration=1.0, formulation=ConservativeFormulation())
                         set!(model, h=1)
                 else
